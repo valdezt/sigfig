aa2f687ef175b2b7e177e15215e2973af76ed011
Travis <drake@pcxftw.com>
2014-01-10 12:56:32 -0500

    Committing v0.1
    Final commit before version 0.1 uploaded

--- README.md
+++
@@ -1,4 +0,0 @@
-sigfig
-======
-
-A soon-to-be python module aimed to aid the user with visual display of scientific numbers, i.e. significant figures, rounding, spacing, etc.
--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     description = 'A library of functions that help present numerical data in a specified format',
     author = 'Travis Valdez',
     author_email = 'drake@pcxftw.com',
-    url = 'https://',
-    download_url = 'https://',
+    url = 'https://github.com/drakegroup/sigfig',
+    download_url = 'https://drakegroup/sigfig/tarball/0.1',
     keywords = ['numbers','numerical','data','output','format']
     classifiers = [],
 )
