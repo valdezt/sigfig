8123d2e0542383c4056e021bec167030771200dd
Travis <drake@pcxftw.com>
2014-01-10 12:44:00 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'sigfig',
+    packages = ['sigfig'],
+    version = '0.1',
+    description = 'A library of functions that help present numerical data in a specified format',
+    author = 'Travis Valdez',
+    author_email = 'drake@pcxftw.com',
+    url = 'https://',
+    download_url = 'https://',
+    keywords = ['numbers','numerical','data','output','format']
+    classifiers = [],
+)
